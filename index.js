5c77c196795803367a2250718b756857fca95bf5
Seth Mwebi <sethmwebi27@gmail.com>
2024-07-05 08:27:24 +0300

    modifies elements lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.querySelector("main#main").remove();
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Seth Mwebi is the champion";
+
